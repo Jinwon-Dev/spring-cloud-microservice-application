4a49de93400c26b38b02cf57e078b6e1c71aaf43
Jinwon-Dev <yjw9424@naver.com>
2025-09-17 23:40:04 +0900

    feat: Spring Cloud Gateway Filter 적용 - Logging Filter(우선 순위 지정)

--- apigateway-service/src/main/java/org/example/apigatewayservice/filter/LoggingFilter.java
+++ apigateway-service/src/main/java/org/example/apigatewayservice/filter/LoggingFilter.java
@@ -3,7 +3,9 @@ package org.example.apigatewayservice.filter;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -17,27 +19,56 @@ public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Co
         super(Config.class);
     }
 
+//    @Override
+//    public GatewayFilter apply(final Config config) {
+//
+//        return (exchange, chain) -> { // exchange -> 요청과 응답에 대한 모든 정보를 담고 있는 객체
+//
+//            final ServerHttpRequest request = exchange.getRequest();
+//            final ServerHttpResponse response = exchange.getResponse();
+//
+//            log.info("Logging Filter baseMessage: {}, {}", config.getBaseMessage(), request.getRemoteAddress());
+//
+//            if (config.isPreLogger()) {
+//                log.info("Logging Filter Start: request uri -> {}", request.getURI());
+//            }
+//
+//            return chain.filter(exchange) // 요청을 다음 필터로 전달
+//                    .then(Mono.fromRunnable(() -> { // 체인의 모든 동작이 완료된 후 실행
+//                        if (config.isPostLogger()) {
+//                            log.info("Logging Filter End: response code -> {}", response.getStatusCode());
+//                        }
+//                    }));
+//        };
+//    }
+
+    /**
+     * 우선 순위를 갖는 Logging Filter 적용
+     */
     @Override
     public GatewayFilter apply(final Config config) {
 
-        return (exchange, chain) -> { // exchange -> 요청과 응답에 대한 모든 정보를 담고 있는 객체
+        GatewayFilter filter = new OrderedGatewayFilter(
+                (exchange, chain) -> { // exchange -> 요청과 응답에 대한 모든 정보를 담고 있는 객체
 
-            final ServerHttpRequest request = exchange.getRequest();
-            final ServerHttpResponse response = exchange.getResponse();
+                    final ServerHttpRequest request = exchange.getRequest();
+                    final ServerHttpResponse response = exchange.getResponse();
 
-            log.info("Logging Filter baseMessage: {}, {}", config.getBaseMessage(), request.getRemoteAddress());
+                    log.info("Logging Filter baseMessage: {}, {}", config.getBaseMessage(), request.getRemoteAddress());
 
-            if (config.isPreLogger()) {
-                log.info("Logging Filter Start: request uri -> {}", request.getURI());
-            }
+                    if (config.isPreLogger()) {
+                        log.info("Logging Filter Start: request uri -> {}", request.getURI());
+                    }
 
-            return chain.filter(exchange) // 요청을 다음 필터로 전달
-                    .then(Mono.fromRunnable(() -> { // 체인의 모든 동작이 완료된 후 실행
-                        if (config.isPostLogger()) {
-                            log.info("Logging Filter End: response code -> {}", response.getStatusCode());
-                        }
-                    }));
-        };
+                    return chain.filter(exchange) // 요청을 다음 필터로 전달
+                            .then(Mono.fromRunnable(() -> { // 체인의 모든 동작이 완료된 후 실행
+                                if (config.isPostLogger()) {
+                                    log.info("Logging Filter End: response code -> {}", response.getStatusCode());
+                                }
+                            }));
+                }, Ordered.HIGHEST_PRECEDENCE);
+
+        return filter;
     }
 
     @Data
