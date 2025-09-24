8e8ced99172d738fcf3d9a5c9a8af51d61e8eea5
Jinwon-Dev <yjw9424@naver.com>
2025-09-24 21:48:29 +0900

    feat: Users Microservice - 인증 처리 구현

--- user-service/src/main/java/org/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/org/example/userservice/security/WebSecurity.java
@@ -5,12 +5,16 @@ import org.example.userservice.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import org.springframework.security.web.util.matcher.IpAddressMatcher;
 
 @Configuration
@@ -20,6 +24,7 @@ public class WebSecurity {
 
     private final UserService userService;
     private final Environment env;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public static final String ALLOWED_IP_ADDRESS = "127.0.0.1";
     public static final String SUBNET = "/32";
@@ -28,15 +33,34 @@ public class WebSecurity {
     @Bean
     protected SecurityFilterChain configure(final HttpSecurity http) throws Exception {
 
+        final AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+
+        final AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2-console/**").permitAll() // 특정 경로 허용
+                        .requestMatchers("/h2-console/**").permitAll()  // 특정 경로 허용
+                        .requestMatchers("/**").access(
+                                new WebExpressionAuthorizationManager(
+                                        "hasIpAddress('127.0.0.1') or hasIpAddress('::1') or " +
+                                                "hasIpAddress('172.30.1.42') or hasIpAddress('::1')")) // host pc ip address
                         .anyRequest().authenticated() // 그 외는 인증 필요
                 )
-                .httpBasic(Customizer.withDefaults()) // Basic 인증 추가
+                .authenticationManager(authenticationManager)
+                .addFilter(getAuthenticationFilter(authenticationManager))
+                .httpBasic(Customizer.withDefaults())  // ← Basic 인증 추가
                 .headers((headers) -> headers
                         .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
 
         return http.build();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter(final AuthenticationManager authenticationManager) throws Exception {
+        final AuthenticationFilter authenticationFilter = new AuthenticationFilter(userService, env, authenticationManager);
+        authenticationFilter.setAuthenticationManager(authenticationManager);
+
+        return authenticationFilter;
     }
 }
--- user-service/src/main/java/org/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/org/example/userservice/service/UserServiceImpl.java
@@ -9,6 +9,8 @@ import org.example.userservice.vo.ResponseOrder;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.env.Environment;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -23,8 +25,23 @@ import java.util.UUID;
 public class UserServiceImpl implements UserService {
 
     private final Environment env;
+
     private final UserRepository userRepository;
+
     private final BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+
+        final UserEntity userEntity = userRepository.findByEmail(username);
+
+        if (userEntity == null)
+            throw new UsernameNotFoundException(username + ": not found");
+
+        return new User(userEntity.getEmail(), userEntity.getEncryptedPwd(),
+                true, true, true, true,
+                new ArrayList<>());
+    }
 
     @Override
     public UserDto createUser(final UserDto userDto) {
@@ -61,5 +78,12 @@ public class UserServiceImpl implements UserService {
     @Override
     public Iterable<UserEntity> getUserByAll() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public UserDto getUserDetailsByEmail(final String email) {
+
+        final UserEntity userEntity = userRepository.findByEmail(email);
+        return new ModelMapper().map(userEntity, UserDto.class);
     }
 }
